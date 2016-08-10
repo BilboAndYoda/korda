644bb1b1bac054353e8510bd9bf38c67daec6f33
Jordan Boudreau <jordboudreau@gmail.com>
2016-08-10 22:33:25 +0000

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,8 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass', '3.2.0.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home'
+ get '/help', to: 'static_pages#help'
+ get '/about', to: 'static_pages#about'
+ get '/contact', to: 'static_pages#contact'
+ get '/signup', to: 'users#new'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
