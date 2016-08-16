644dce04e6ceaa69b1166b788e481bf2f395eae9
BilboAndYoda <jordboudreau@gmail.com>
2016-08-16 20:55:33 +0000

    add email after signup fix 1"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,17 @@ class UsersController < ApplicationController
     if @user.save
       flash[:success] = "Welcome to the Korda!"
       # Tell the UserMailer to send a welcome email after save
-      UserMailer.welcome_email(@user).deliver_later
+        UserMailer.welcome_email(@user).deliver_later
  
       format.html { redirect_to(@user, notice: 'User was successfully created.') }
       format.json { render json: @user, status: :created, location: @user }
+      redirect_to @user
     else
       format.html { render action: 'new' }
       format.json { render json: @user.errors, status: :unprocessable_entity }
-    end
+    end    
   end
+
   
   private
 
